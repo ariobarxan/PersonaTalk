8af3000b5504a1c4634c6596a4cd0e53177ddb60
Ario <ariobarxan.dev@gmail.com>
2025-05-03 20:43:45 +1000

    Initial Commit

---
+++ PersonaTalk/PersonaTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonaTalkApp.swift
+//  PersonaTalk
+//
+//  Created by Ario Liyan on 3/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct PersonaTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
